f82deecb08ccade8e98472c565b3b4cf8ebec6b3
Emmanuel <emmanueljucaban@gmail.com>
2018-08-12 13:45:38 -0700

    Add Stripe payments functionality.

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
+import Payments from './Payments';
+
 class Header extends Component {
   renderContent() {
     switch(this.props.auth) {
@@ -12,9 +14,10 @@ class Header extends Component {
           <li><a href="/auth/google">Login with google </a></li>
         );
       default:
-        return (
-          <li><a href="/api/logout">Logout</a></li>
-        );
+        return [
+          <li key='1'><Payments/></li>,
+          <li key='2'><a href="/api/logout">Logout</a></li>
+        ];
     }
   }
   render() {
@@ -26,9 +29,7 @@ class Header extends Component {
             className='left brand-logo'
           >Emailer</Link>
           <ul className='right'>
-            <li>
               { this.renderContent() }
-            </li>
           </ul>
         </div>
       </nav>
