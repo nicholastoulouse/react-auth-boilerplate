de9c4c5ff9b76dc0c23f519471e029ab39afff20
Emmanuel Jucaban <EmmanuelJucaban@gmail.com>
2018-08-12 22:24:04 -0700

    Created heroku settings

---
+++ config/prod.js
@@ -0,0 +1,8 @@
+module.exports = {
+  cookieKey             : process.env.cookieKey,
+  googleClientId        : process.env.googleClientId,
+  googleClientSecret    : process.env.googleClientSecret,
+  mongoURI              : process.env.mongoURI,
+  stripePublishableKey  : process.env.stripePublishableKey,
+  stripeSecretKey       : process.env.stripeSecretKey,
+};
