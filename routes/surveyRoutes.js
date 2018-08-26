67508b8b005ddcf1d9c9600057db063e1df935a3
Emmanuel <emmanueljucaban@gmail.com>
2018-08-26 16:12:22 -0700

    Create webhook route for updating the survey model

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,3 +1,6 @@
+const _               = require('lodash');
+const Path            = require('path-parser').default;
+const { URL }         = require('url');
 const mongoose        = require('mongoose');
 const requireLogin    = require('./../middlewares/requireLogin');
 const requireCredits  = require('../middlewares/requireCredits');
@@ -13,12 +16,12 @@ module.exports = app => {
   });
 
   app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
-    const { title, subject, body, recipients } = req.body;
+    const {title, subject, body, recipients} = req.body;
     const survey = new Survey({
       title,
       subject,
       body,
-      recipients: recipients.split(',').map(email => ({ email: email.trim() })),
+      recipients: recipients.split(',').map(email => ({email: email.trim()})),
       _user: req.user.id,
       dateSent: Date.now()
     });
@@ -31,14 +34,43 @@ module.exports = app => {
       req.user.credits -= 1;
       const user = await req.user.save();
       res.send(user);
-    } catch(e) {
+    } catch (e) {
       res.status(422).send(e);
     }
   });
 
   app.post('/api/surveys/webhooks', (req, res) => {
-    console.log(req.body);
+    const p = new Path('/api/surveys/:surveyId/:choice');
+    _.chain(req.body)
+      .map(({email, url}) => {
+        const match = p.test(new URL(url).pathname);
+        if (match) {
+          return {email, surveyId: match.surveyId, choice: match.choice};
+        }
+      })
+      .compact()
+      .uniqBy('email', 'surveyId')
+      .each(({surveyId, email, choice}) => {
+        try {
+          Survey.updateOne(
+            {
+              _id: surveyId,
+              recipients: {
+                $elemMatch: {email: email, responded: false}
+              }
+            },
+            {
+              $inc: {[choice]: 1},
+              $set: {'recipients.$.responded': true}
+            }
+          ).exec();
+        } catch(e) {
+          console.log(e);
+        }
+
+      })
+      .value();
     res.send({});
-  })
+  });
 
 };
